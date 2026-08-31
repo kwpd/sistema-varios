08e38b137ee0b78135d95b85b51b3fa0e861b7f7
kwpd <hou_18@msn.com>
2026-08-31 12:13:31 -0500

    Update database configuration in config.php

--- PanaGamers/config.php
+++ PanaGamers/config.php
@@ -1,6 +1,6 @@
 <?php
-$host = 'peak.evolution-host.com';
-$dbname = 's451_SkillPointsBeta';
-$username = 'u451_I2EqpJTTR4';
-$password = 'jDwx!V8OHzv@MWPV1Lts6.Pk';
+$host = 'localhost.com';
+$dbname = 'basedata';
+$username = 'name';
+$password = 'password';
 ?>
